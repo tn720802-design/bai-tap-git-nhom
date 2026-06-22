dc54b6f35e15820ed1f534a5cdd3562b4bc70170
Trong Minh Vo <tn720802@gmail.com>
2026-06-22 16:27:37 +0700

    Revise comment for tax calculation function
    Updated comment to reflect versioning and reviewer input.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def tinh_thue_thu_nhap(thu_nhap):
-    # Dev B áp dụng mức thuế 10%
+    # Phiên bản main - Dev B áp dụng mức thuế 10% (đã được duyệt)
+    # Cập nhật bởi Reviewer
     return thu_nhap * 0.1
 
 print("Thuế phải nộp của bạn là:", tinh_thue_thu_nhap(10000000))
